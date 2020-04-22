3b93640a9b4721eb707a626ce8a8d7f61cd27965
Crowdbotics <team@crowdbotics.com>
2020-04-22 18:45:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/retro_build_16153/MainActivity.java
@@ -0,0 +1,15 @@
+package com.retro_build_16153;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "retro_build_16153";
+    }
+}
